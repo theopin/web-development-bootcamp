39b63801b301151fc3638f7505ac1fc1099862e5
theopin <theopinto98@hotmail.com>
2023-09-19 19:42:54 +0800

    Add routing for frontend

--- 45 NFT/opend/src/opend_assets/src/components/App.jsx
+++ 45 NFT/opend/src/opend_assets/src/components/App.jsx
@@ -3,15 +3,15 @@ import Header from "./Header";
 import Footer from "./Footer";
 import "bootstrap/dist/css/bootstrap.min.css";
 import homeImage from "../../assets/home-img.png";
-import Minter from "./Minter";
+
+
+
 function App() {
-  const NftId = "rkp4c-7iaaa-aaaaa-aaaca-cai"
+
   return (
     <div className="App">
-      <Header />
-      <Minter />
-      {/* <img className="bottom-space" src={homeImage} /> */}
-      <Footer />
+      <Header/>
+      <Footer/>
     </div>
   );
 }
--- 45 NFT/opend/src/opend_assets/src/components/Gallery.jsx
+++ 45 NFT/opend/src/opend_assets/src/components/Gallery.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import Item from "./Item";
 
-function Gallery() {
+function Gallery(props) {
+  const NftId = "rkp4c-7iaaa-aaaaa-aaaca-cai"
+
   return (
     <div className="gallery-view">
-      <h3 className="makeStyles-title-99 Typography-h3">Discover</h3>
+      <h3 className="makeStyles-title-99 Typography-h3">{props.title}</h3>
       <div className="disGrid-root disGrid-container disGrid-spacing-xs-2">
         <div className="disGrid-root disGrid-item disGrid-grid-xs-12">
           <div className="disGrid-root disGrid-container disGrid-spacing-xs-5 disGrid-justify-content-xs-center"></div>
-          <Item />
+          <Item  id={NftId}/>
         </div>
       </div>
     </div>
